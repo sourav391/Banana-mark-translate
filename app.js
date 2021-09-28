06a08b5db121a638997c7e4d4a71275549282cb6
sourav391 <souravpatil31@gmail.com>
2021-09-28 17:37:28 +0530

    style : css changes

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtinput = document.querySelector("#txt-input");
-var outputdiv = document.querySelector("#output")
+console.log('connected!')
+const translateBtn = document.querySelector('#translateBtn')
+const inpText = document.querySelector('.inpText')
+const outputSection = document.querySelector('.output')
+
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
 
 
+// tells the button what to do when clicked 
+// main task - translate and show in output
+function clickHandler() {
+    const inputText = inpText.value
+    // server processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        const translatedText = json.contents.translated;
+        outputSection.innerText = translatedText
+    })
+    .catch(errorHandling)
+}
+
+function errorHandling(err) {
+    console.log('Error occured - ', err)
+    alert('Server Issue, please try again after sometime. For error, check console')
+}
 
 
-
-function clickhandler() {
-    outputdiv.innerText = "sadasdsadsadas " + txtinput.value;
-
-
-};
-
-
-
-
-btntranslate.addEventListener("click", clickhandler)
+translateBtn.addEventListener('click', clickHandler)
